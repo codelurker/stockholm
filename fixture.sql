7b347d7e6467073635a6a9dec4803cbc0ac8e220
Roberto Cosenza <robcos@robcos.com>
2010-10-06 19:48:57 +0100

    Added correct stops for portfolio 2

--- fixture.sql
+++ fixture.sql
@@ -22,7 +22,16 @@ INSERT INTO position VALUES
 ('LUPE.ST',    'SEK', 1, '2010-09-28',         null,     55,    null,  99, null,     2400,  53.5, 1),
 ('AAPL',       'USD', 1, '2010-07-05',         null, 259.98,    null, 109, null,       15,   234, 1),
 ('AXIS.ST',    'SEK', 1, '2010-09-22',         null,     92,    null,  99, null,      450,    85, 1),
-('BOL.ST',     'SEK', 1, '2010-09-14',         null,     97.5,  null,  99, null,      250, 83.95, 1),
-('ERIC-A.ST',     'SEK', 1, '2010-09-14',         null,     97.5,  null,  99, null,      250, 83.95, 1),
-('KARO.ST',     'SEK', 1, '2010-09-14',         null,     97.5,  null,  99, null,      250, 83.95, 1)
+('BOL.ST',     'SEK', 1, '2010-09-14',         null,     97.5,  null,  99, null,      250, 83.95, 1)
+;
+-- ('ERIC-A.ST',     'SEK', 1, '2010-09-14',         null,     97.5,  null,  99, null,      250, 83.95, 1),
+-- ('KARO.ST',     'SEK', 1, '2010-09-14',         null,     97.5,  null,  99, null,      250, 83.95, 1)
+INSERT INTO position VALUES 
+('FUNDS',      'SEK', 1, '2000-01-01',         null,      1,    null,   0,    0,  2508.50,      0, 2),
+('CASH',       'SEK', 1, '2000-01-01',         null,      1,    null,   0,    0, 17747.97,      0, 2),
+-- ('HOLM-B.ST',  'SEK', 1, '2010-09-09',         null, 217.00,    null,  99, null,       25,   602, 2),
+('HMB.ST',     'SEK', 1, '2010-09-02',         null, 247.10,    null,  99, null,      40, 235.3, 2),
+('LUPE.ST',    'SEK', 1, '2010-07-23',         null,     40,    null,  99, null,      130,  28.30, 2),
+('TLSN.ST',    'SEK', 1, '2010-07-23',         null,  53.65,    null,  99, null,      100,     49, 2),
+('TEL2-A.ST',  'SEK', 1, '2010-07-19',         null,  129.0,    null,  99, null,       40, 117.30, 2)
 ;
