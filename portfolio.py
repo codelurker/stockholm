54160681502b3f8c7ac5401a9da4d966c45ddad9
Roberto Cosenza <robcos@robcos.com>
2010-10-08 22:36:16 +0100

    Refactoring portfolio

--- portfolio.py
+++ portfolio.py
@@ -1,30 +1,49 @@
 from dao import *
 from decimal import *
 
-getcontext().prec = 8
-portfolio = Portfolio.get_portfolio(1)
-positions = portfolio.positions
 def f(arg):
   if arg == True:
     return "Yes"
-  if arg == False:
-    return ""
-  try:
-    float(arg)
-    return "%6.2f" % arg
-  except TypeError:
-    return ""
-  except ValueError:
+  if arg is False:
+    return "No"
+  #try:
+  if(isinstance(arg, Decimal)):
+    return "%8.2f" % arg
+  else:
     return arg
+  #except TypeError:
+  #  return ""
+  #except ValueError:
+  #  return arg
 
 def ff(tpl):
   return tuple(map(f, tpl))
 
-print "%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s" % ('Symbol', 'Value', 'Gain', 'Shares', 'RTR', 'Close', 'Lowest low 20 day')
-print "-------------------------------------------------------------------------------------------------------------------"
-for p in positions:
-  print "%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s" % ff(
-    (p.symbol, p.get_value(portfolio.currency), p.get_gain(), 
-    p.shares, p.get_rtr(), p.current_quote.close, p.get_trailing_stop(), p.should_sell()))
-print "-------------------------------------------------------------------------------------------------------------------"
-print "TOTAL\t\t%s" % (f(portfolio.get_value()))
+def print_portfolio(id):
+  portfolio = Portfolio.get_portfolio(id)
+  positions = portfolio.positions
+  print "-------------------------------------------------------------------------------------------------------------------"
+  print "Symbol\tDate\tValue\tGain\tShares\tRTR\tClose\tStop\tC Stop\tT Stop\tSell"
+  print "-------------------------------------------------------------------------------------------------------------------"
+  for p in positions:
+    print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % ff(
+      (
+      p.symbol, 
+      p.current_quote.date,
+      p.get_value(portfolio.currency), 
+      p.get_gain(), 
+      p.shares, 
+      p.get_rtr(), 
+      p.current_quote.close, 
+      p.stop,
+      p.get_stop(),
+      p.get_trailing_stop(),
+      p.should_sell()
+      ))
+  print "-------------------------------------------------------------------------------------------------------------------"
+  print "TOTAL\t\t%s" % (f(portfolio.get_value()))
+  print ""
+  
+print_portfolio(1)
+print_portfolio(2)
+print_portfolio(3)
