f5daa8c09ad1fdf2a9777a2038ee3d7aa704ae6a
Roberto Cosenza <robcos@robcos.com>
2010-10-10 18:00:03 +0100

    Scripts to run the whole procedure Dump in and ou the db before running tests

---
+++ run_all.sh
@@ -0,0 +1,7 @@
+mysql -u root stocks  -v -f <quotes.sql
+mysql -u root stocks  -v -f <indicators.sql
+mysql -u root stocks  -v -f <positions.sql
+mysql -u root stocks  -v -f <fixture.sql
+python download.py
+python indicators.py
+python portfolio.py | expand -t 12
--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,4 @@
+mysqldump -u root stocks >dump.sql
 mysql -u root stocks < fixture.sql
 python dao_test.py
+mysqldump -u root stocks <dump.sql >/dev/null
