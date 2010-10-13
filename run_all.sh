be4a57f4f7ce3c4d615feca68ee615002cb9a097
Roberto Cosenza <robcos@robcos.com>
2010-10-13 18:25:35 +0100

    Don't load text fixtures when running portfolio

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 mysql -u root stocks  -v -f <quotes.sql
 mysql -u root stocks  -v -f <indicators.sql
 mysql -u root stocks  -v -f <positions.sql
-mysql -u root stocks  -v -f <fixture.sql
+mysql -u root stocks  -v -f <portfolio.sql
 python download.py
 python indicators.py
 python portfolio.py | expand -t 12
