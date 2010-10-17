b97548dd7928db3d36b184a59b48ace48848eb30
Roberto Cosenza <robcos@robcos.com>
2010-10-17 14:53:30 +0100

    Download footsie 350

--- run_all.sh
+++ run_all.sh
@@ -3,6 +3,11 @@ mysql -u root stocks  -v -f <indicators.sql
 mysql -u root stocks  -v -f <portfolios.sql
 mysql -u root stocks  -v -f <positions.sql
 mysql -u root stocks  -v -f <my_portfolios.sql
-python download.py
+python otherstocks.py
+python omx-large-cap.py
+python omx-mid-cap.py
+python omx30.py
+python footsie350.py
+
 python indicators.py
 python portfolio.py | expand -t 12
