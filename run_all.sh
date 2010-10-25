a679e696d92914adc28703f22e6f5907dda64b18
Roberto Cosenza <robcos@robcos.com>
2010-10-25 21:56:28 +0100

    Run really all

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,14 @@
-mysql -u root stocks  -v -f <quotes.sql
-mysql -u root stocks  -v -f <indicators.sql
-mysql -u root stocks  -v -f <portfolios.sql
-mysql -u root stocks  -v -f <positions.sql
-mysql -u root stocks  -v -f <my_portfolios.sql
+#mysql -u root stocks  -v -f <quotes.sql
+#mysql -u root stocks  -v -f <indicators.sql
+#mysql -u root stocks  -v -f <portfolios.sql
+#mysql -u root stocks  -v -f <positions.sql
+#mysql -u root stocks  -v -f <my_portfolios.sql
 python otherstocks.py
 python omx-large-cap.py
 python omx-mid-cap.py
 python omx30.py
-python footsie350.py
+python footsie-350.py
 
 python indicators.py
+mysqldump -u root stocks >dump.sql
 python portfolio.py | expand -t 12
