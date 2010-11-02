d34580c1b7d5c2c01090d10e36481e79c1730b97
Roberto Cosenza <robcos@robcos.com>
2010-11-02 15:32:09 +0000

    Print list of charts in one line so we can open it from command line with a browser

--- find_entries.py
+++ find_entries.py
@@ -9,31 +9,27 @@ def find(account_value, filter):
   print "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
   print "Symbol\tDate\tClose\tStop\tTarget\tShares\tPosition\tChart"
   print "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+  charts = []
   for quote in dao.Quote.get_latest_quotes():
     if not re.search(filter, quote.symbol):
       continue
     if quote.is_above_20_day_high():
       
       if(quote.is_above_sma20() and quote.is_above_sma50()):
-        #print "Above sma20 and sma50"
         chart = "http://finance.yahoo.com/echarts?s=%s" % quote.symbol
-        #print "https://www.avanza.se/aza/sok/standard.jsp?word=%s" % (
-        #    quote.symbol.replace('.ST', '').replace('-A', '').replace('-B', ''))
+        charts.append(chart)
 
         stop = quote.get_indicator().calculate_stop(quote.close) #TODO add transaction cost
         risk = quote.close - stop
         target = risk * 6 + quote.close
         shares = dao.Position.get_shares(quote, account_value/risk_factor)
         position = long(shares * quote.close)
-        print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (quote.symbol, quote.date, quote.close, stop, target, shares, position, chart)
-        #chart = chart + "+" + quote.symbol
-    else:
-      #print "Leave %s " % quote.symbol
-      pass
-  print
+        print "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (quote.symbol, quote.date, quote.close, stop, target, shares, position)
+  print 
+  print ' '.join(charts)
 
-print "AVANZA pension"
+print "\Pension"
 find(Decimal('48000'), '\.ST')
 
-print "STOCKS"
-find(Decimal('390000') / dao.Currency.get_rate('GBPSEK'), '\.ST')
+print "\nStocks"
+find(Decimal('390000') / dao.Currency.get_rate('GBPSEK'), '\.L')
